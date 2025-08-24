9feb09fb838ad9cdaf47908c99bac2c53a77b2bd
enzosdev <enzohfs@gmail.com>
2025-08-24 20:56:38 -0300

    feat: This interface calls an external api using feign client

--- src/main/java/dev/java/ecommerce/basketservice/client/PlatziStoreClient.java
+++ src/main/java/dev/java/ecommerce/basketservice/client/PlatziStoreClient.java
@@ -1,4 +1,22 @@
 package dev.java.ecommerce.basketservice.client;
 
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+//anotacao do feign client para fazer chamadas http para outros servicos
+//nome do cliente e url do servico
+//url do servico vem do application.yml
+@FeignClient(name = "platzi", url = "${basket.client.platzi}")
 public interface PlatziStoreClient {
+
+    @GetMapping("/products")
+    List<PlatziProductResponse> getAllProducts();
+
+    @GetMapping("/products/{id}")
+     PlatziProductResponse getProductById(@PathVariable Long id);
 }
