0ed1b82c56787073ece113e9991b21a248c2d354
enzosdev <enzohfs@gmail.com>
2025-08-24 22:14:19 -0300

    feat: add to productService the annotation cacheable on the methods, to storing the request on redis

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,6 +3,7 @@ package dev.java.ecommerce.basketservice.service;
 
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +17,14 @@ public class ProductService {
         this.platziStoreClient = platziStoreClient;
     }
 
+    @Cacheable(value ="products")
     public List<PlatziProductResponse> getAllProducts() {
         return platziStoreClient.getAllProducts();
     }
 
-    public PlatziProductResponse getAllProductById(Long id) {
-        return platziStoreClient.getProductById(id);
+
+    @Cacheable(value = "products", key = "#productId")
+    public PlatziProductResponse getAllProductById(Long productId) {
+        return platziStoreClient.getProductById(productId);
     }
 }
