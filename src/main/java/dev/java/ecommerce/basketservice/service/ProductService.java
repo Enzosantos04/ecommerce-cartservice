7ba484fae8fd3352a946fb07776836188297897a
enzosdev <enzohfs@gmail.com>
2025-09-07 13:14:21 -0300

    feat: Adiciona log.info para checar as requisicoes

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,11 +3,13 @@ package dev.java.ecommerce.basketservice.service;
 
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductService {
 
@@ -25,6 +27,7 @@ public class ProductService {
 
     @Cacheable(value = "products", key = "#productId")
     public PlatziProductResponse getAllProductById(Long productId) {
+        log.info("Fetching product with id {}", productId);
         return platziStoreClient.getProductById(productId);
     }
 }
