485da06a71f9a3b61e1eb711e93dc44b1699fdf6
enzosdev <enzohfs@gmail.com>
2025-09-07 23:12:43 -0300

    feat: Repository para entity Basket usando mongoRepository

--- src/main/java/dev/java/ecommerce/basketservice/repository/BasketRepository.java
+++ src/main/java/dev/java/ecommerce/basketservice/repository/BasketRepository.java
@@ -1,4 +1,11 @@
 package dev.java.ecommerce.basketservice.repository;
 
-public interface BasketRepository {
+import dev.java.ecommerce.basketservice.entity.Basket;
+import dev.java.ecommerce.basketservice.enums.Status;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+//mongo repository faz abstrcao das queries do banco de dados
+//tendo os metodos basicos de CRUD
+public interface BasketRepository extends MongoRepository<Basket, String> {
+    Optional<Basket> findByClientAndStatus(Long client, Status status) ;
 }
