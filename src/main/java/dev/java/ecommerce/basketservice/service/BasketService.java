1323878facede301b744b1b7608716f69533537f
enzosdev <enzohfs@gmail.com>
2025-09-07 23:10:24 -0300

    feat: adiciona a regra de negocio para criar um basket e getBasketById

--- src/main/java/dev/java/ecommerce/basketservice/service/BasketService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/BasketService.java
@@ -1,4 +1,59 @@
 package dev.java.ecommerce.basketservice.service;
 
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import dev.java.ecommerce.basketservice.controller.request.BasketRequest;
+import dev.java.ecommerce.basketservice.entity.Basket;
+import dev.java.ecommerce.basketservice.entity.Product;
+import dev.java.ecommerce.basketservice.enums.Status;
+import dev.java.ecommerce.basketservice.repository.BasketRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BasketService {
+
+    private final BasketRepository basketRepository;
+    private final ProductService productService;
+
+    public BasketService(BasketRepository basketRepository, ProductService productService){
+        this.basketRepository = basketRepository;
+        this.productService = productService;
+    }
+
+    public Basket createBasket(BasketRequest basketRequest) {
+        basketRepository.findByClientAndStatus(basketRequest.clientId(), Status.OPEN)
+                .ifPresent(basket -> {
+                    throw new RuntimeException("There is already an open basket for this client");
+                });
+
+
+        List<Product> products = new ArrayList<>();
+        //para cada produto na requisição, buscar os detalhes do produto no serviço de produtos
+        basketRequest.products().forEach(productRequest ->{
+            PlatziProductResponse platziProductResponse = productService.getAllProductById(productRequest.id());
+            products.add(Product.builder()
+                    .id(platziProductResponse.id())
+                    .title(platziProductResponse.title())
+                    .price(platziProductResponse.price())
+                    .quantity(productRequest.quantity())
+                    .build());
+        } );
+        //constroi o objeto do tipo Basket
+        //.build() cria o objeto
+        Basket basket = Basket.builder()
+                .client(basketRequest.clientId())
+                .status(Status.OPEN)
+                .products(products)
+                .build();
+        basket.calculateTotalPrice();
+        return basketRepository.save(basket);
+    }
+
+    public Basket getBasketById(String id){
+        return basketRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Basket not found"));
+
+    }
 }
