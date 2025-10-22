17bd0062de7ba8137c8fb2f5079fb40f29525d31
enzosdev <enzohfs@gmail.com>
2025-10-22 13:17:38 -0300

    feat: paymentRequest receiving the enum paymentMethod

--- src/main/java/dev/java/ecommerce/basketservice/controller/request/PaymentRequest.java
+++ src/main/java/dev/java/ecommerce/basketservice/controller/request/PaymentRequest.java
@@ -1,4 +1,10 @@
 package dev.java.ecommerce.basketservice.controller.request;
+import dev.java.ecommerce.basketservice.enums.PaymentMethod;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PaymentRequest {
+    private PaymentMethod paymentMethod;
 }
