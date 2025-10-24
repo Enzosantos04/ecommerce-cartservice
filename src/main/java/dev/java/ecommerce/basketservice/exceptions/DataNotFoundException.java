d5c1bf9f920759955720eb5fba88c9824269059e
enzosdev <enzohfs@gmail.com>
2025-10-24 17:06:23 -0300

    feat: ccreating DataNotFounException that throws an exception message

--- src/main/java/dev/java/ecommerce/basketservice/exceptions/DataNotFoundException.java
+++ src/main/java/dev/java/ecommerce/basketservice/exceptions/DataNotFoundException.java
@@ -1,4 +1,7 @@
 package dev.java.ecommerce.basketservice.exceptions;
 
-public class DataNotForundException {
+public class DataNotFoundException extends RuntimeException{
+    public DataNotFoundException(String message){
+        super(message);
+    }
 }
