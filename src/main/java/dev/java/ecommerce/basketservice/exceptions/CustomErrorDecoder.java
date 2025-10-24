c54230bdbe0ce8152fd91ac20a1129245e0c5f4d
enzosdev <enzohfs@gmail.com>
2025-10-24 17:03:28 -0300

    feat: CustomErrorDecoder class for identify if the present error is 400 if yes, return the exception DataNotFoundException

--- src/main/java/dev/java/ecommerce/basketservice/exceptions/CustomErrorDecoder.java
+++ src/main/java/dev/java/ecommerce/basketservice/exceptions/CustomErrorDecoder.java
@@ -1,4 +1,16 @@
 package dev.java.ecommerce.basketservice.exceptions;
 
-public class CustomErrorDecoder {
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String methodKey, Response response){
+        switch(response.status()){
+            case 400:
+                return new DataNotFoundException("Product not Found");
+            default:
+                return new Exception("Exception while getting product");
+        }
+    }
 }
