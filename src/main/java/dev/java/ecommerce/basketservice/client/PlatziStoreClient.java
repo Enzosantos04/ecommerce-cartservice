0cacbd5d6ef65f9b76ff90b01a2d624bbbab1985
enzosdev <enzohfs@gmail.com>
2025-10-24 17:01:22 -0300

    feat: adding configuration and CustomErrorDecoder.class on feignClient annotation for get error from platzi Response

--- src/main/java/dev/java/ecommerce/basketservice/client/PlatziStoreClient.java
+++ src/main/java/dev/java/ecommerce/basketservice/client/PlatziStoreClient.java
@@ -1,8 +1,8 @@
 package dev.java.ecommerce.basketservice.client;
 
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import dev.java.ecommerce.basketservice.exceptions.CustomErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,7 +11,7 @@ import java.util.List;
 //anotacao do feign client para fazer chamadas http para outros servicos
 //nome do cliente e url do servico
 //url do servico vem do application.yml
-@FeignClient(name = "platzi", url = "${basket.client.platzi}")
+@FeignClient(name = "platzi", url = "${basket.client.platzi}", configuration = {CustomErrorDecoder.class} )
 public interface PlatziStoreClient {
 
     @GetMapping("/products")
